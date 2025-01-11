e1a90a0dd3f1500922526aa0c7a930bbf9098fec
MKKosmowski <mikolaj.kosmowski@onet.pl>
2025-01-11 22:05:08 +0100

    database setup

---
+++ src/database/dbConnect.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.error("Failed to connect to MongoDB:", error);
+		process.exit(1);
+	}
+};
+
+module.exports = dbConnect;
