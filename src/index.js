eac914abe316e3b160bf98dddc5100dd1eb2af9d
MKKosmowski <mikolaj.kosmowski@onet.pl>
2025-01-18 11:09:27 +0100

    express setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
 const dbConnect = require("./database/dbConnect");
 
-// Configs and initialization
+// Configs
+const app = express();
 dbConnect();
+
+// Midlewares
+app.use(express.json());
+
+// Routes
+app.get("/api/test", (req, res) => {
+	return res.status(200).send({ msg: "Test approved" });
+});
+
+// Start listening
+const PORT = process.env.EXPRESS_PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
