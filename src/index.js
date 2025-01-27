1444adf7bf3468c708813b4f89c1c17923c4e374
MKKosmowski <mikolaj.kosmowski@onet.pl>
2025-01-27 17:30:31 +0100

    vercel setup

--- src/index.js
+++ src/index.js
@@ -16,8 +16,13 @@ app.use(express.json());
 // Routers
 app.use(mainRouter);
 
-// Tests
+// Error handlers
+app.use((req, res) => {
+	res.status(404).send({ error: "Ta ścieżka nie istnieje" });
+});
 
 // Start listening
 const PORT = process.env.EXPRESS_PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
